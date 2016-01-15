6f47e974d1046f141c7c5a311f1694f3ad07417b
Ben Winjum <benjum@benjum.local>
2016-01-15 13:32:11 -0800

    Update old files in mppic2 (via V. Decyk)

--- openmp_mpi/mppic2/mppush2_h.f90
+++ openmp_mpi/mppic2/mppush2_h.f90
@@ -7,8 +7,9 @@
          subroutine PDICOMP2L(edges,nyp,noff,nypmx,nypmn,ny,kstrt,nvp,  &
      &idps)
          implicit none
-         integer :: nyp, noff, nypmx, nypmn, ny, kstrt, nvp, idps
-         real, dimension(idps) :: edges
+         integer, intent(in) :: ny, kstrt, nvp, idps
+         integer, intent(inout) :: nyp, noff, nypmx, nypmn
+         real, dimension(idps), intent(inout) :: edges
          end subroutine
       end interface
 !
@@ -16,11 +17,12 @@
          subroutine PDISTR2(part,edges,npp,nps,vtx,vty,vdx,vdy,npx,npy, &
      &nx,ny,idimp,npmax,idps,ipbc,ierr)
          implicit none
-         integer :: npp, nps, npx, npy, nx, ny, idimp, npmax, idps, ipbc
-         integer :: ierr
-         real :: vtx, vty, vdx, vdy
-         real, dimension(idimp,npmax) :: part
-         real, dimension(idps) :: edges
+         integer, intent(in) :: nps, npx, npy, nx, ny, idimp, npmax
+         integer, intent(in) :: idps, ipbc
+         integer, intent(inout) :: npp, ierr
+         real, intent(in) :: vtx, vty, vdx, vdy
+         real, dimension(idimp,npmax), intent(inout) :: part
+         real, dimension(idps), intent(in) :: edges
          end subroutine
       end interface
 !
@@ -28,10 +30,11 @@
          subroutine PPDBLKP2L(part,kpic,npp,noff,nppmx,idimp,npmax,mx,my&
      &,mx1,mxyp1,irc)
          implicit none
-         integer :: nppmx, idimp, npmax, mx, my, mx1, mxyp1, irc
-         integer :: npp, noff
-         real, dimension(idimp,npmax) :: part
-         integer, dimension(mxyp1) :: kpic
+         integer, intent(in) :: idimp, npmax, mx, my, mx1, mxyp1, npp
+         integer, intent(in) :: noff
+         integer, intent(inout) :: nppmx, irc
+         real, dimension(idimp,npmax), intent(in) :: part
+         integer, dimension(mxyp1), intent(inout) :: kpic
          end subroutine
       end interface
 !
@@ -39,11 +42,12 @@
          subroutine PPPMOVIN2L(part,ppart,kpic,npp,noff,nppmx,idimp,    &
      &npmax,mx,my,mx1,mxyp1,irc)
          implicit none
-         integer :: nppmx, idimp, npmax, mx, my, mx1, mxyp1, irc
-         integer :: npp, noff
-         real, dimension(idimp,npmax) :: part
-         real, dimension(idimp,nppmx,mxyp1) :: ppart
-         integer, dimension(mxyp1) :: kpic
+         integer, intent(in) :: nppmx, idimp, npmax, mx, my, mx1, mxyp1
+         integer, intent(in) :: npp, noff
+         integer, intent(inout) :: irc
+         real, dimension(idimp,npmax), intent(in) :: part
+         real, dimension(idimp,nppmx,mxyp1), intent(inout) :: ppart
+         integer, dimension(mxyp1), intent(inout) :: kpic
          end subroutine
       end interface
 !
@@ -51,10 +55,11 @@
          subroutine PPPCHECK2L(ppart,kpic,noff,nyp,idimp,nppmx,nx,mx,my,&
      &mx1,myp1,irc)
          implicit none
-         integer :: idimp, nppmx, nx, mx, my, mx1, myp1, irc
-         integer :: noff, nyp
-         real, dimension(idimp,nppmx,mx1*myp1) :: ppart
-         integer, dimension(mx1*myp1) :: kpic
+         integer, intent(in) :: idimp, nppmx, nx, mx, my, mx1, myp1
+         integer, intent(in) :: noff, nyp
+         integer, intent(inout) :: irc
+         real, dimension(idimp,nppmx,mx1*myp1), intent(in) :: ppart
+         integer, dimension(mx1*myp1), intent(in) :: kpic
          end subroutine
       end interface
 !
@@ -62,13 +67,13 @@
          subroutine PPGPPUSH2L(ppart,fxy,kpic,noff,nyp,qbm,dt,ek,nx,ny, &
      &mx,my,idimp,nppmx,nxv,nypmx,mx1,mxyp1,ipbc)
          implicit none
-         integer :: nx, ny, mx, my, idimp, nppmx, nxv, nypmx, mx1, mxyp1
-         integer :: ipbc
-         integer :: noff, nyp
-         real :: qbm, dt, ek
-         real, dimension(idimp,nppmx,mxyp1) :: ppart
-         real, dimension(2,nxv,nypmx) :: fxy
-         integer, dimension(mxyp1) :: kpic
+         integer, intent(in) :: noff, nyp, nx, ny, mx, my, idimp, nppmx
+         integer, intent(in) :: nxv, nypmx, mx1, mxyp1, ipbc
+         real, intent(in) :: qbm, dt
+         real, intent(inout) :: ek
+         real, dimension(idimp,nppmx,mxyp1), intent(inout) :: ppart
+         real, dimension(2,nxv,nypmx), intent(in) :: fxy
+         integer, dimension(mxyp1), intent(in) :: kpic
          end subroutine
       end interface
 !
@@ -76,28 +81,29 @@
          subroutine PPGPPUSHF2L(ppart,fxy,kpic,ncl,ihole,noff,nyp,qbm,dt&
      &,ek,nx,ny,mx,my,idimp,nppmx,nxv,nypmx,mx1,mxyp1,ntmax,irc)
          implicit none
-         integer :: nx, ny, mx, my, idimp, nppmx, nxv, nypmx, mx1, mxyp1
-         integer :: ntmax, irc
-         integer :: noff, nyp
-         real :: qbm, dt, ek
-         real, dimension(idimp,nppmx,mxyp1) :: ppart
-         real, dimension(2,nxv,nypmx) :: fxy
-         integer, dimension(mxyp1) :: kpic
-         integer, dimension(8,mxyp1) :: ncl
-         integer, dimension(2,ntmax+1,mxyp1) :: ihole
+         integer, intent(in) :: noff, nyp, nx, ny, mx, my, idimp, nppmx
+         integer, intent(in) :: nxv, nypmx, mx1, mxyp1, ntmax
+         integer, intent(inout) :: irc
+         real, intent(in) :: qbm, dt
+         real, intent(inout) :: ek
+         real, dimension(idimp,nppmx,mxyp1), intent(inout) :: ppart
+         real, dimension(2,nxv,nypmx), intent(in) :: fxy
+         integer, dimension(mxyp1), intent(in) :: kpic
+         integer, dimension(8,mxyp1), intent(inout) :: ncl
+         integer, dimension(2,ntmax+1,mxyp1), intent(inout) :: ihole
          end subroutine
       end interface
 !
       interface
          subroutine PPGPPOST2L(ppart,q,kpic,noff,qm,idimp,nppmx,mx,my,  &
-     &nxv,nypmx,mx1,mxyp1)
+     &nxv, nypmx,mx1,mxyp1)
          implicit none
-         integer :: idimp, nppmx, mx, my, nxv, nypmx, mx1, mxyp1
-         integer :: noff
-         real :: qm
-         real, dimension(idimp,nppmx,mxyp1) :: ppart
-         real, dimension(nxv,nypmx) :: q
-         integer, dimension(mxyp1) :: kpic
+         integer, intent(in) :: noff, idimp, nppmx, mx, my, nxv, nypmx
+         integer, intent(in) :: mx1, mxyp1
+         real, intent(in) :: qm
+         real, dimension(idimp,nppmx,mxyp1), intent(in) :: ppart
+         real, dimension(nxv,nypmx), intent(inout) :: q
+         integer, dimension(mxyp1), intent(in) :: kpic
          end subroutine
       end interface
 !
@@ -106,16 +112,16 @@
      &ncll,nclr,noff,nyp,idimp,nppmx,nx,ny,mx,my,mx1,myp1,npbmx,ntmax,  &
      &nbmax,irc)
          implicit none
-         integer :: idimp, nppmx, nx, ny, mx, my, mx1, myp1
-         integer :: npbmx, ntmax, nbmax, irc
-         integer :: noff, nyp
-         real, dimension(idimp,nppmx,mx1*myp1) :: ppart
-         real, dimension(idimp,npbmx,mx1*myp1) :: ppbuff
-         real, dimension(idimp,nbmax) :: sbufl, sbufr
-         integer, dimension(mx1*myp1) :: kpic
-         integer, dimension(8,mx1*myp1) :: ncl
-         integer, dimension(2,ntmax+1,mx1*myp1) :: ihole
-         integer, dimension(3,mx1) :: ncll, nclr
+         integer, intent(in) :: idimp, nppmx, nx, ny, mx, my, mx1, myp1
+         integer, intent(in) :: npbmx, ntmax, nbmax, noff, nyp
+         integer, intent(inout) :: irc
+         real, dimension(idimp,nppmx,mx1*myp1), intent(inout) :: ppart
+         real, dimension(idimp,npbmx,mx1*myp1), intent(inout) :: ppbuff
+         real, dimension(idimp,nbmax), intent(inout) :: sbufl, sbufr
+         integer, dimension(mx1*myp1), intent(in) :: kpic
+         integer, dimension(8,mx1*myp1), intent(inout) :: ncl
+         integer, dimension(2,ntmax+1,mx1*myp1), intent(inout) :: ihole
+         integer, dimension(3,mx1), intent(inout) :: ncll, nclr
          end subroutine
       end interface
 !
@@ -123,13 +129,15 @@
          subroutine PPPORDERF2LA(ppart,ppbuff,sbufl,sbufr,ncl,ihole,ncll&
      &,nclr,idimp,nppmx,mx1,myp1,npbmx,ntmax,nbmax,irc)
          implicit none
-         integer :: idimp, nppmx, mx1, myp1, npbmx, ntmax, nbmax, irc
-         real, dimension(idimp,nppmx,mx1*myp1) :: ppart
-         real, dimension(idimp,npbmx,mx1*myp1) :: ppbuff
-         real, dimension(idimp,nbmax) :: sbufl, sbufr
-         integer, dimension(8,mx1*myp1) :: ncl
-         integer, dimension(2,ntmax+1,mx1*myp1) :: ihole
-         integer, dimension(3,mx1) :: ncll, nclr
+         integer, intent(in) :: idimp, nppmx, mx1, myp1, npbmx, ntmax
+         integer, intent(in) :: nbmax
+         integer, intent(inout) :: irc
+         real, dimension(idimp,nppmx,mx1*myp1), intent(inout) :: ppart
+         real, dimension(idimp,npbmx,mx1*myp1), intent(inout) :: ppbuff
+         real, dimension(idimp,nbmax), intent(inout) :: sbufl, sbufr
+         integer, dimension(8,mx1*myp1), intent(inout) :: ncl
+         integer, dimension(2,ntmax+1,mx1*myp1), intent(in) :: ihole
+         integer, dimension(3,mx1), intent(inout) :: ncll, nclr
          end subroutine
       end interface
 !
@@ -137,30 +145,32 @@
          subroutine PPPORDER2LB(ppart,ppbuff,rbufl,rbufr,kpic,ncl,ihole,&
      &mcll,mclr,idimp,nppmx,mx1,myp1,npbmx,ntmax,nbmax,irc)
          implicit none
-         integer :: idimp, nppmx, mx1, myp1, npbmx, ntmax, nbmax, irc
-         real, dimension(idimp,nppmx,mx1*myp1) :: ppart
-         real, dimension(idimp,npbmx,mx1*myp1) :: ppbuff
-         real, dimension(idimp,nbmax) :: rbufl, rbufr
-         integer, dimension(mx1*myp1) :: kpic
-         integer, dimension(8,mx1*myp1) :: ncl
-         integer, dimension(2,ntmax+1,mx1*myp1) :: ihole
-         integer, dimension(3,mx1) :: mcll, mclr
+         integer, intent(in) :: idimp, nppmx, mx1, myp1, npbmx, ntmax
+         integer, intent(in) :: nbmax
+         integer, intent(inout) :: irc
+         real, dimension(idimp,nppmx,mx1*myp1), intent(inout) :: ppart
+         real, dimension(idimp,npbmx,mx1*myp1), intent(in) :: ppbuff
+         real, dimension(idimp,nbmax), intent(in) :: rbufl, rbufr
+         integer, dimension(mx1*myp1), intent(inout) :: kpic
+         integer, dimension(8,mx1*myp1), intent(in) :: ncl
+         integer, dimension(2,ntmax+1,mx1*myp1), intent(in) :: ihole
+         integer, dimension(3,mx1), intent(in) :: mcll, mclr
          end subroutine
       end interface
 !
       interface
          subroutine PPCGUARD2XL(fxy,nyp,nx,ndim,nxe,nypmx)
          implicit none
-         integer :: nyp, nx, ndim, nxe, nypmx
-         real, dimension(ndim,nxe,nypmx) :: fxy
+         integer, intent(in) :: nyp, nx, ndim, nxe, nypmx
+         real, dimension(ndim,nxe,nypmx), intent(inout) :: fxy
          end subroutine
       end interface
 !
       interface
          subroutine PPAGUARD2XL(q,nyp,nx,nxe,nypmx)
          implicit none
-         integer :: nyp, nx, nxe, nypmx
-         real, dimension(nxe,nypmx) :: q
+         integer, intent(in) :: nyp, nx, nxe, nypmx
+         real, dimension(nxe,nypmx), intent(inout) :: q
          end subroutine
       end interface
 !
@@ -168,20 +178,21 @@
          subroutine MPPOIS22(q,fxy,isign,ffc,ax,ay,affp,we,nx,ny,kstrt, &
      &nyv,kxp,nyhd)
          implicit none
-         integer :: isign, nx, ny, kstrt, nyv, kxp, nyhd
-         real :: ax, ay, affp, we
-         complex, dimension(nyv,kxp) :: q
-         complex, dimension(2,nyv,kxp) :: fxy
-         complex, dimension(nyhd,kxp) :: ffc
+         integer, intent(in) :: isign, nx, ny, kstrt, nyv, kxp, nyhd
+         real, intent(in) :: ax, ay, affp
+         real, intent(inout) :: we
+         complex, dimension(nyv,kxp), intent(in)  :: q
+         complex, dimension(2,nyv,kxp), intent(inout) :: fxy
+         complex, dimension(nyhd,kxp), intent(inout) :: ffc
          end subroutine
       end interface
 !
       interface
          subroutine WPFFT2RINIT(mixup,sct,indx,indy,nxhyd,nxyhd)
          implicit none
-         integer :: indx, indy, nxhyd, nxyhd
-         integer, dimension(nxhyd) :: mixup
-         complex, dimension(nxyhd) :: sct
+         integer, intent(in) :: indx, indy, nxhyd, nxyhd
+         integer, dimension(nxhyd), intent(inout) :: mixup
+         complex, dimension(nxyhd), intent(inout) :: sct
          end subroutine
       end interface
 !
@@ -189,14 +200,14 @@
          subroutine WPPFFT2RM(f,g,bs,br,isign,ntpose,mixup,sct,ttp,indx,&
      &indy,kstrt,nvp,nxvh,nyv,kxp,kyp,kypd,nxhyd,nxyhd)
          implicit none
-         integer :: isign, ntpose, indx, indy, kstrt, nvp, nxvh, nyv
-         integer :: kxp, kyp, kypd, nxhyd, nxyhd
-         real :: ttp
-         real, dimension(2*nxvh,kypd) :: f
-         complex, dimension(nyv,kxp) :: g
-         complex, dimension(kxp,kyp) :: bs, br
-         integer, dimension(nxhyd) :: mixup
-         complex, dimension(nxyhd) :: sct
+         integer, intent(in) :: isign, ntpose, indx, indy, kstrt, nvp
+         integer, intent(in) :: nxvh, nyv, kxp, kyp, kypd, nxhyd, nxyhd
+         real, intent(inout) :: ttp
+         real, dimension(2*nxvh,kypd), intent(inout) :: f
+         complex, dimension(nyv,kxp), intent(inout) :: g
+         complex, dimension(kxp,kyp), intent(inout) :: bs, br
+         integer, dimension(nxhyd), intent(in) :: mixup
+         complex, dimension(nxyhd), intent(in) :: sct
          end subroutine
       end interface
 !
@@ -204,14 +215,14 @@
          subroutine WPPFFT2RM2(f,g,bs,br,isign,ntpose,mixup,sct,ttp,indx&
      &,indy,kstrt,nvp,nxvh,nyv,kxp,kyp,kypd,nxhyd,nxyhd)
          implicit none
-         integer :: isign, ntpose, indx, indy, kstrt, nvp, nxvh, nyv
-         integer :: kxp, kyp, kypd, nxhyd, nxyhd
-         real :: ttp
-         real, dimension(2,2*nxvh,kypd) :: f
-         complex, dimension(2,nyv,kxp) :: g
-         complex, dimension(2,kxp,kyp) :: bs, br
-         integer, dimension(nxhyd) :: mixup
-         complex, dimension(nxyhd) :: sct
+         integer, intent(in) :: isign, ntpose, indx, indy, kstrt, nvp
+         integer, intent(in) :: nxvh, nyv, kxp, kyp, kypd, nxhyd, nxyhd
+         real, intent(inout) :: ttp
+         real, dimension(2,2*nxvh,kypd), intent(inout) :: f
+         complex, dimension(2,nyv,kxp), intent(inout) :: g
+         complex, dimension(2,kxp,kyp), intent(inout) :: bs, br
+         integer, dimension(nxhyd), intent(in) :: mixup
+         complex, dimension(nxyhd), intent(in) :: sct
          end subroutine
       end interface
 !
@@ -219,11 +230,11 @@
          subroutine PPFFT2RMXX(f,isign,mixup,sct,indx,indy,kstrt,kypi,  &
      &kypp,nxvh,kypd,nxhyd,nxyhd)
          implicit none
-         integer :: isign, indx, indy, kstrt, nxvh, kypi, kypp, kypd
-         integer :: nxhyd, nxyhd
-         real, dimension(2*nxvh,kypd) :: f
-         integer, dimension(nxhyd) :: mixup
-         complex, dimension(nxyhd) :: sct
+         integer, intent(in) :: isign, indx, indy, kstrt, kypi, kypp
+         integer, intent(in) :: nxvh, kypd, nxhyd, nxyhd
+         real, dimension(2*nxvh,kypd), intent(inout) :: f
+         integer, dimension(nxhyd), intent(in) :: mixup
+         complex, dimension(nxyhd), intent(in) :: sct
          end subroutine
       end interface
 !
@@ -231,11 +242,11 @@
          subroutine PPFFT2RMXY(g,isign,mixup,sct,indx,indy,kstrt,kxpi,  &
      &kxpp,nyv,kxp,nxhyd,nxyhd)
          implicit none
-         integer :: isign, indx, indy, kstrt, kxpi, kxpp, nyv, kxp
-         integer :: nxhyd, nxyhd
-         complex, dimension(nyv,kxp) :: g
-         integer, dimension(nxhyd) :: mixup
-         complex, dimension(nxyhd) :: sct
+         integer, intent(in) :: isign, indx, indy, kstrt, kxpi, kxpp
+         integer, intent(in) :: nyv, kxp, nxhyd, nxyhd
+         complex, dimension(nyv,kxp), intent(inout) :: g
+         integer, dimension(nxhyd), intent(in) :: mixup
+         complex, dimension(nxyhd), intent(in) :: sct
          end subroutine
       end interface
 !
@@ -243,11 +254,11 @@
          subroutine PPFFT2RM2XX(f,isign,mixup,sct,indx,indy,kstrt,kypi, &
      &kypp,nxvh,kypd,nxhyd,nxyhd)
          implicit none
-         integer :: isign, indx, indy, kstrt, nxvh, kypi, kypp, kypd
-         integer :: nxhyd, nxyhd
-         real, dimension(3,2*nxvh,kypd) :: f
-         integer, dimension(nxhyd) :: mixup
-         complex, dimension(nxyhd) :: sct
+         integer, intent(in) :: isign, indx, indy, kstrt, kypi, kypp
+         integer, intent(in) :: nxvh, kypd, nxhyd, nxyhd
+         real, dimension(2,2*nxvh,kypd), intent(inout) :: f
+         integer, dimension(nxhyd), intent(in) :: mixup
+         complex, dimension(nxyhd), intent(in) :: sct
          end subroutine
       end interface
 !
@@ -255,11 +266,11 @@
          subroutine PPFFT2RM2XY(g,isign,mixup,sct,indx,indy,kstrt,kxpi, &
      &kxpp,nyv,kxp,nxhyd,nxyhd)
          implicit none
-         integer :: isign, indx, indy, kstrt, kxpi, kxpp, nyv, kxp
-         integer :: nxhyd, nxyhd
-         complex, dimension(3,nyv,kxp) :: g
-         integer, dimension(nxhyd) :: mixup
-         complex, dimension(nxyhd) :: sct
+         integer, intent(in) :: isign, indx, indy, kstrt, kxpi, kxpp
+         integer, intent(in) :: nyv, kxp, nxhyd, nxyhd
+         complex, dimension(2,nyv,kxp), intent(inout) :: g
+         integer, dimension(nxhyd), intent(in) :: mixup
+         complex, dimension(nxyhd), intent(in) :: sct
          end subroutine
       end interface
 !
